4fc50df9d8643b71e8f3cf6649067f66ee474758
fudasatomi <imotas.aduf@gmail.com>
2020-02-05 14:30:43 +0900

    3Rails

---
+++ 3Rails/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '3e3542986f5a3f85f32046e11fa8fc2a59b2663381a96f896a98a8e85e131866981f665bf1507777c465be4d4f7a24916a8d5130faf9d8585f6c77c9b5b78930'
